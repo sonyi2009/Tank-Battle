fffd2555efa119ead121ad5799e3e9cb6ab45bbe
宋毅 <31555603+sonyi2009@users.noreply.github.com>
2018-10-28 13:36:44 +0800

    坦克大战
    Q版坦克大战

---
+++ TanksProject/Assets/Scripts/TankHealth.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TankHealth : MonoBehaviour {
+
+    public int hp = 100;
+    public GameObject tankExplosion;
+    public AudioClip tankExplosionAudio;
+
+    public Slider hpSlider;
+
+    private int hpTotal;
+
+	// Use this for initialization
+	void Start () {
+	    hpTotal = hp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void TakeDamage() {
+        if (hp <= 0) return;
+        hp -= Random.Range(10, 20);
+        hpSlider.value = (float)hp /hpTotal;
+        if (hp <= 0) {//收到伤害之后 血量为0 控制死亡效果
+            AudioSource.PlayClipAtPoint(tankExplosionAudio,transform.position);
+            GameObject.Instantiate(tankExplosion, transform.position + Vector3.up, transform.rotation);
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
